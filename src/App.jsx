198688afade3fe12ce325b4ae761ffdbe179ae60
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-08-04 16:36:09 +0500

    Develop Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./Components/Search";
 
 const App = ()=>{
+  const [searchTerm,setSearchTerm]=useState('');
   return(
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main>
+
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> you'll Enjoy Without Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+    </main>
   )
 }
 
